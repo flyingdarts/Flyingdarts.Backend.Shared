96d58fa24acc7cf69e7461c5c31aa69719baafa1
Mike Pattyn <mike.pattyn1@gmail.com>
2023-10-07 13:02:29 +0200

    update

--- Metadata.cs
+++ Metadata.cs
@@ -5,7 +5,7 @@ public class Metadata
     public GameDto Game { get; set; }
     public IOrderedEnumerable<PlayerDto> Players { get; set; }
     public Dictionary<string, List<DartDto>> Darts { get; set; }
-
+    public long NextPlayer { get; set; }
     public Dictionary<string, object> toDictionary()
     {
         var result = new Dictionary<string, object>
