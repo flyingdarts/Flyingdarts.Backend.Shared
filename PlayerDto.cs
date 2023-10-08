eae11037013961a0024938e3ae23ef63fed39dcb
Mike Pattyn <mike.pattyn1@gmail.com>
2023-10-08 03:32:03 +0200

    Update

--- PlayerDto.cs
+++ PlayerDto.cs
@@ -6,4 +6,6 @@ public class PlayerDto
     public string PlayerName { get; set; }
     public string CreatedAt { get; set; }
     public string Country { get; set; }
+    public string Sets { get; set; }
+    public string Legs { get; set; }
 }
